26b75af8adbaa5077083dd8fbb977cb26abd702e
cerodriguezz <141981427+cerodriguezz@users.noreply.github.com>
2023-09-28 17:10:07 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,14 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  var result = (fahren-32) * (5/9);
+  return result;
+};
+var mytemp = convertToCelsius(64);
+
+createMessage
+console.log("My tempature in celcius is",mytemp,"degrees")
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -19,17 +26,34 @@ function convertToCelsius(fahren) {}
  *   < 86   |  "warm"
  *   < 100  |  "hot"
  *
- * @param {number} fahren
- * @param {number} celc
+ * @param {64} fahren
+ * @param {17.778} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  window.alert("temp is 17.778 c feels very cold")
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(rand) {
+  let x;
+  let y;
+  let z;
+  
+  document.getElementById ("generatebutton") .onclick (rand)
+    x = math.floor(math.rand()*6) +1;
+    y = math.floor(math.rand()*6) +1;
+    z = math.floor(math.rand()*6) +1;
+
+   document.getElementById("xlabel").innerHTML =x;
+   document.getElementById("ylabel").innerHTML =y;
+   document.getElementById("zlabel").innerHTML =z;
+
+  }
+
 
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
